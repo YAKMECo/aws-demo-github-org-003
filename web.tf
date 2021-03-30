8c1a651ba5d6cb85969ee5ecfc8b2a1762ee292b
miaugertg <michael+miaugertggithub@lm4k.com>
2021-03-30 09:24:34 -0400

    Change comment

--- web.tf
+++ web.tf
@@ -18,7 +18,7 @@ resource "aws_security_group" "acme_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  # HTTP access from the VPC - changed
+  # HTTP access from the VPC
   ingress {
     from_port   = 80
     to_port     = 80
